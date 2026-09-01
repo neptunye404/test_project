2193e7511bb713d19fff63aaf339172f89c983c2
Neptunye <m4tt30.tt@gmail.com>
2026-09-01 20:04:19 +0200

    Update main.py
    Added a dictionary for meme translations and user input handling.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+meme_dict = {
+    "CRINGE": "Roblox New Gen",
+    "LOL": "Mirtilli",
+}
+
+parola = input("Scrivi una parola che non capisci (usa solo lettere maiuscole!): ")
+
+if parola in meme_dict.keys():
+    if parola == "CRINGE":
+        print("Roblox New Gen")
+    elif parola == "LOL":
+        print("Mirtilli")
+else:
+    print("La parola non è stata trovata");
