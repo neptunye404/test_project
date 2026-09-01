041439bdfb70ce6f28a28f083b5fc774488eec93
Neptunye <m4tt30.tt@gmail.com>
2026-09-01 19:49:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-meme_dict = {
-    "CRINGE": "Roblox New Gen",
-    "LOL": "Mirtilli",
-}
-
-parola = input("Scrivi una parola che non capisci (usa solo lettere maiuscole!): ")
-
-if parola in meme_dict.keys():
-    if parola == "CRINGE":
-        print("Roblox New Gen")
-    elif parola == "LOL":
-        print("Mirtilli")
-else:
-    print("La parola non è stata trovata");
+print("Hello World")
